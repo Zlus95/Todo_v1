6667459c63b1d75294a94e847fb450de0b600504
APavlov <sologod@yandex.ru>
2025-03-01 22:35:04 +0300

    getTasks added

---
+++ backend/handlers/tasksHandlers.go
@@ -0,0 +1,31 @@
+package handlers
+
+import (
+	"backend/models"
+	"context"
+	"encoding/json"
+	"net/http"
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+var collection *mongo.Collection
+
+func InitTaskHandlers(c *mongo.Collection) {
+	collection = c
+}
+
+func GetTasks(w http.ResponseWriter, r *http.Request) {
+	var tasks []models.Task
+	ctx, cancel := context.WithTimeout(r.Context(), 10*time.Second)
+	defer cancel()
+	cursor, _ := collection.Find(ctx, bson.M{})
+	for cursor.Next(ctx) {
+		var task models.Task
+		cursor.Decode(&task)
+		tasks = append(tasks, task)
+	}
+	json.NewEncoder(w).Encode(tasks)
+}
