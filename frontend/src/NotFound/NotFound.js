b57f5fa9820d626ec1a9bd177681703819c445ec
APavlov <sologod@yandex.ru>
2025-03-02 11:50:41 +0300

    page not found

--- frontend/src/NotFound/NotFound.js
+++ frontend/src/NotFound/NotFound.js
@@ -1,5 +1,18 @@
-import React from "react";
+import React, { memo } from "react";
+import { useNavigate } from "react-router-dom";
+import "./style.css";
 
-const NotFound = () => <div>dsds</div>;
+const NotFound = () => {
+  const navigate = useNavigate();
 
-export default NotFound;
+  return (
+    <div className="Сontainer">
+      <p className="Text">Page not found</p>
+      <button className="Button" onClick={() => navigate(-1)}>
+        back
+      </button>
+    </div>
+  );
+};
+
+export default memo(NotFound);
