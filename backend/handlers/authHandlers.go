fc92b0bc6c6fd5434f17c663aa3687e8f14852c6
APavlov <sologod@yandex.ru>
2025-03-02 21:19:45 +0300

    login done

--- backend/handlers/authHandlers.go
+++ backend/handlers/authHandlers.go
@@ -7,9 +7,9 @@ import (
 	"net/http"
 	"time"
 
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"golang.org/x/crypto/bcrypt"
-	// "go.mongodb.org/mongo-driver/bson"
 )
 
 var userCollection *mongo.Collection
@@ -38,4 +38,28 @@ func Register(w http.ResponseWriter, r *http.Request) {
 
 	w.WriteHeader(http.StatusCreated)
 	json.NewEncoder(w).Encode(map[string]string{"message": "User registered successfully"})
+}
+
+func Login(w http.ResponseWriter, r *http.Request) {
+	var user models.User
+	if err := json.NewDecoder(r.Body).Decode(&user); err != nil {
+		http.Error(w, "Invalid request body", http.StatusBadRequest)
+		return
+	}
+
+	var currentUser models.User
+	ctx, cancel := context.WithTimeout(r.Context(), 10*time.Second)
+	defer cancel()
+	if err := userCollection.FindOne(ctx, bson.M{"email": user.Email}).Decode(&currentUser); err != nil {
+		http.Error(w, "Invalid credentials", http.StatusUnauthorized)
+		return
+	}
+
+	if err := bcrypt.CompareHashAndPassword([]byte(currentUser.Password), []byte(user.Password)); err != nil {
+		http.Error(w, "Invalid credentials", http.StatusUnauthorized)
+		return
+	}
+
+	token, _ := utils.GenerateJWT(currentUser.ID.Hex())
+	json.NewEncoder(w).Encode(map[string]string{"token": token})
 }
