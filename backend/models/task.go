9fa7c6994866971cac742cb0dd60abfe5974ecd3
APavlov <sologod@yandex.ru>
2025-03-02 20:59:06 +0300

    user model

--- backend/models/task.go
+++ backend/models/task.go
@@ -4,7 +4,7 @@ import "go.mongodb.org/mongo-driver/bson/primitive"
 
 type Task struct {
 	ID primitive.ObjectID `json:"id,omitempty" bson:"_id,omitempty"`
-	// UserID primitive.ObjectID `json:"user_id" bson:"user_id"`
+	UserID primitive.ObjectID `json:"user_id" bson:"user_id"`
 	Title string `json:"title" bson:"title"`
 	Done  bool   `json:"done" bson:"done"`
 }
